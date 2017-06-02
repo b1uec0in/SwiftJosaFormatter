0829bd2d4ca7ef975d561c660e96ddc17159da06
Bae Yong-Ju <1100388@SKTX1100388MN01.local>
2017-06-02 16:40:32 +0900

    닉네임 입력 샘플 추가

--- Example/JosaFormatter/ViewController.swift
+++ Example/JosaFormatter/ViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import UIKit
+import JosaFormatter
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nickNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func onOkButtonTouchUp(_ sender: Any) {
+        var message = "닉네임을 입력하세요."
+        
+        if let text = nickNameTextField.text {
+            if text.length > 0 {
+                message = KoreanUtils.format("'%@'를 입력했습니다.", text)
+            }
+        }
+        
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
